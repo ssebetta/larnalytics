0d601004ce70cb5e58ad10d56b6b9e7a0b138412
harold <haroldpeter6@gmail.com>
2024-07-12 14:42:30 +0300

    Add chart data analysis

--- src/Http/Controllers/AnalyticsController.php
+++ src/Http/Controllers/AnalyticsController.php
@@ -2,9 +2,12 @@
 // src/Http/Controllers/AnalyticsController.php
 namespace Ssebetta\Larnalytics\Http\Controllers;
 
+use Carbon\Carbon;
+use DateTime;
 use Ssebetta\Larnalytics\Models\PageView;
 use Ssebetta\Larnalytics\Models\CustomEvent;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class AnalyticsController extends BaseController
 {
@@ -12,7 +15,86 @@ class AnalyticsController extends BaseController
     {
         $pageViews = PageView::orderByDesc('id')->simplePaginate(15);
         $totalPageViews = PageView::count();
-        return view('larnalytics::page_views', compact('pageViews', 'totalPageViews'));
+
+        // Prepare data for charts
+        $last24HoursData = $this->getChartData('hour', Carbon::now()->subDay());
+        $lastWeekData = $this->getChartData('day', Carbon::now()->subWeek());
+        $lastMonthData = $this->getChartData('week', Carbon::now()->subMonth());
+        $lastYearData = $this->getChartData('month', Carbon::now()->subYear());
+        $deviceData = $this->getDeviceData();
+
+        return view('larnalytics::page_views', compact(
+            'pageViews',
+            'totalPageViews',
+            'last24HoursData',
+            'lastWeekData',
+            'lastMonthData',
+            'lastYearData',
+            'deviceData'
+        ));
+    }
+
+    private function getChartData($interval, $startTime)
+    {
+        $pageViews = PageView::where('viewed_at', '>=', $startTime)->get();
+        $data = [];
+        $labels = [];
+
+        switch ($interval) {
+            case 'hour':
+                $pageViews = $pageViews->groupBy(function ($view) {
+                    return Carbon::parse($view->viewed_at)->format('H');
+                });
+                $format = 'H';
+                $labelFormat = 'H'; // Format for labels
+                break;
+            case 'day':
+                $pageViews = $pageViews->groupBy(function ($view) {
+                    return Carbon::parse($view->viewed_at)->format('D-d');
+                });
+                $format = 'D-d';
+                $labelFormat = 'D-d'; // Format for labels
+                break;
+            case 'week':
+                $pageViews = $pageViews->groupBy(function ($view) {
+                    return Carbon::parse($view->viewed_at)->format('M-d');
+                });
+                $format = 'M-d';
+                $labelFormat = 'M-d'; // Format for labels
+                break;
+            case 'month':
+                $pageViews = $pageViews->groupBy(function ($view) {
+                    return Carbon::parse($view->viewed_at)->format('M-Y');
+                });
+                $format = 'M-Y';
+                $labelFormat = 'M-Y'; // Format for labels
+                break;
+            default:
+                throw new \InvalidArgumentException("Invalid interval specified.");
+        }
+
+        foreach ($pageViews as $group => $views) {
+            // Parse the date string using DateTime
+            $parsedDate = DateTime::createFromFormat($format, $group);
+            if (!$parsedDate) {
+                throw new \InvalidArgumentException("Invalid date format: $group");
+            }
+            $labels[] = $parsedDate->format($labelFormat);
+            $data[] = count($views);
+        }
+
+        return ['labels' => $labels, 'data' => $data];
+    }
+
+
+    private function getDeviceData()
+    {
+        $deviceCounts = PageView::select('device', DB::raw('count(*) as total'))
+                                ->groupBy('device')
+                                ->get();
+        $labels = $deviceCounts->pluck('device');
+        $data = $deviceCounts->pluck('total');
+        return ['labels' => $labels, 'data' => $data];
     }
 
     public function showCustomEvents()
--- src/resources/views/page_views.blade.php
+++ src/resources/views/page_views.blade.php
@@ -30,6 +30,7 @@
             color: #6a0dad;
             text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
         }
+        .text-purple { color: #6a0dad !important; }
         .flip {
             animation: flipInY 3.5s ease;
         }
@@ -54,60 +55,250 @@
                 transform: perspective(400px);
             }
         }
+        .chart-container { position: relative; height: 50vh; width: 100vw; }
     </style>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-dark">
-    <a class="navbar-brand" href="#">Larnalytics</a>
-</nav>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">Larnalytics</a>
+    </nav>
 
-<div class=" mt-4">
-    <div class="text-center">
-        <h3 class="mb-3">{{ config('app.name') }}</h3>
-        <p id="visitsCount" class="visits-count">{{ $totalPageViews }} visits</p>
-    </div>
-    <table class="table table-responsive table-striped table-bordered">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>User</th>
-                <th>Page URL</th>
-                <th>Viewed At</th>
-                <th>IP Address</th>
-                <th>Location</th>
-                <th>User Agent</th>
-                <th>Device</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($pageViews as $view)
+    <div class=" mt-4">
+        <div class="text-center">
+            <h3 class="mb-3">{{ config('app.name') }}</h3>
+            <p id="visitsCount" class="visits-count">{{ $totalPageViews }} visits</p>
+        </div>
+
+        <ul class="nav nav-tabs mt-3" id="pageViewTabs" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active text-purple" id="tab24hrs-tab" data-toggle="tab" href="#tab24hrs" role="tab" aria-controls="tab24hrs" aria-selected="true">Last 24 Hours</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-purple" id="tabPastWeek-tab" data-toggle="tab" href="#tabPastWeek" role="tab" aria-controls="tabPastWeek" aria-selected="false">Past Week</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-purple" id="tabPastMonth-tab" data-toggle="tab" href="#tabPastMonth" role="tab" aria-controls="tabPastMonth" aria-selected="false">Past Month</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-purple" id="tabPastYear-tab" data-toggle="tab" href="#tabPastYear" role="tab" aria-controls="tabPastYear" aria-selected="false">Past Year</a>
+            </li>
+        </ul>
+        <div class="tab-content mt-3" id="pageViewTabsContent">
+            <div class="tab-pane fade show active" id="tab24hrs" role="tabpanel" aria-labelledby="tab24hrs-tab">
+                <div class="d-flex justify-content-center align-items-center" style="min-height: 400px;">
+                    <canvas id="last24HoursChart"></canvas>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="tabPastWeek" role="tabpanel" aria-labelledby="tabPastWeek-tab">
+                <div class="d-flex justify-content-center align-items-center" style="min-height: 400px;">
+                    <canvas id="lastWeekChart"></canvas>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="tabPastMonth" role="tabpanel" aria-labelledby="tabPastMonth-tab">
+                <div class="d-flex justify-content-center align-items-center" style="min-height: 400px;">
+                    <canvas id="lastMonthChart"></canvas>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="tabPastYear" role="tabpanel" aria-labelledby="tabPastYear-tab">
+                <div class="d-flex justify-content-center align-items-center" style="min-height: 400px;">
+                    <canvas id="lastYearChart"></canvas>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-center align-items-center">
+            <h3 class="">Devices</h3>
+            <div class="col-lg-8 chart-container mt-3">
+                <canvas id="deviceChart"></canvas>
+            </div>
+        </div>
+        <table class="table table-responsive table-striped table-bordered mt-3">
+            <thead>
                 <tr>
-                    <td>{{ $view->id }}</td>
-                    <td>{{ $view->user_id }}</td>
-                    <td>{{ $view->page_url }}</td>
-                    <td>{{ $view->viewed_at }}</td>
-                    <td>{{ $view->ip_address }}</td>
-                    <td>{{ $view->location }}</td>
-                    <td>{{ $view->user_agent }}</td>
-                    <td>{{ $view->device }}</td>
+                    <th>ID</th>
+                    <th>User</th>
+                    <th>Page URL</th>
+                    <th>Viewed At</th>
+                    <th>IP Address</th>
+                    <th>Location</th>
+                    <th>User Agent</th>
+                    <th>Device</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <div class="d-flex justify-content-center">
-        {{ $pageViews->links() }}
+            </thead>
+            <tbody>
+                @foreach ($pageViews as $view)
+                    <tr>
+                        <td>{{ $view->id }}</td>
+                        <td>{{ $view->user_id }}</td>
+                        <td>{{ $view->page_url }}</td>
+                        <td>{{ $view->viewed_at }}</td>
+                        <td>{{ $view->ip_address }}</td>
+                        <td>{{ $view->location }}</td>
+                        <td>{{ $view->user_agent }}</td>
+                        <td>{{ $view->device }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <div class="d-flex justify-content-center">
+            {{ $pageViews->links() }}
+        </div>
     </div>
-</div>
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script>
-    // Add flip animation on page load
-    window.onload = function() {
-        document.getElementById('visitsCount').classList.add('flip');
-    }
-</script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        // page_views.js
+
+        document.addEventListener('DOMContentLoaded', function () {
+            var pageViewTabs = new bootstrap.Tab(document.getElementById('pageViewTabs'));
+            var tabContent = document.getElementById('pageViewTabsContent');
+
+            // Handle tab click event
+            pageViewTabs.show();
+
+            // Render charts based on active tab
+            document.querySelectorAll('.nav-link').forEach(function (tab) {
+                tab.addEventListener('click', function (e) {
+                    e.preventDefault();
+                    var tabId = this.getAttribute('href').replace('#', '');
+                });
+            });
+        });
+    </script>
+    <script>
+        const last24HoursData = {!! json_encode($last24HoursData) !!};
+        const lastWeekData = {!! json_encode($lastWeekData) !!};
+        const lastMonthData = {!! json_encode($lastMonthData) !!};
+        const lastYearData = {!! json_encode($lastYearData) !!};
+        const deviceData = {!! json_encode($deviceData) !!};
+
+        const last24HoursCtx = document.getElementById('last24HoursChart').getContext('2d');
+        new Chart(last24HoursCtx, {
+            type: 'line',
+            data: {
+                labels: last24HoursData.labels,
+                datasets: [{
+                    label: 'Last 24 Hours',
+                    data: last24HoursData.data,
+                    backgroundColor: 'rgba(106, 13, 173, 0.2)',
+                    borderColor: 'rgba(106, 13, 173, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        const lastWeekCtx = document.getElementById('lastWeekChart').getContext('2d');
+        new Chart(lastWeekCtx, {
+            type: 'line',
+            data: {
+                labels: lastWeekData.labels,
+                datasets: [{
+                    label: 'Last Week',
+                    data: lastWeekData.data,
+                    backgroundColor: 'rgba(106, 13, 173, 0.2)',
+                    borderColor: 'rgba(106, 13, 173, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        const lastMonthCtx = document.getElementById('lastMonthChart').getContext('2d');
+        new Chart(lastMonthCtx, {
+            type: 'bar',
+            data: {
+                labels: lastMonthData.labels,
+                datasets: [{
+                    label: 'Last Month',
+                    data: lastMonthData.data,
+                    backgroundColor: 'rgba(106, 13, 173, 0.2)',
+                    borderColor: 'rgba(106, 13, 173, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        const lastYearCtx = document.getElementById('lastYearChart').getContext('2d');
+        new Chart(lastYearCtx, {
+            type: 'bar',
+            data: {
+                labels: lastYearData.labels,
+                datasets: [{
+                    label: 'Last Year',
+                    data: lastYearData.data,
+                    backgroundColor: 'rgba(106, 13, 173, 0.2)',
+                    borderColor: 'rgba(106, 13, 173, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        const deviceCtx = document.getElementById('deviceChart').getContext('2d');
+        new Chart(deviceCtx, {
+            type: 'doughnut',
+            data: {
+                labels: deviceData.labels,
+                datasets: [{
+                    label: 'Devices',
+                    data: deviceData.data,
+                    backgroundColor: [
+                        'rgba(106, 13, 173, 0.2)',
+                        'rgba(106, 13, 173, 0.4)',
+                        'rgba(106, 13, 173, 0.6)'
+                    ],
+                    borderColor: [
+                        'rgba(106, 13, 173, 1)',
+                        'rgba(106, 13, 173, 1)',
+                        'rgba(106, 13, 173, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+    <script>
+        // Add flip animation on page load
+        window.onload = function() {
+            document.getElementById('visitsCount').classList.add('flip');
+        }
+    </script>
 </body>
 </html>
